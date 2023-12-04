5067a84256c49f1a6cf1827154dbaec4cae9dd81
seokcheon <my980519@gmail.com>
2023-12-04 12:49:31 +0900

    start welcomes.py

---
+++ docs/welcomes.py
@@ -0,0 +1,17 @@
+## 변수 선언 후 정의 시 고려졈 (넣는 값이 문자인지 숫자인지 명확해야함)
+# 문자 출력
+helloworld = "Hello, World! Seokcheon Moon" # 문자형 변수
+print(helloworld)
+
+# 숫자 합산 출력
+numbers = 3 + 5 # 숫자형 변수
+print(numbers)
+
+# 데이터 타입 : 문자형 or 숫자형 등 변수에 대한 정의
+
+# 가정문
+print("numbers")
+if 5 > 2: # 묶음 기호인 :와 tab은 하나의 쌍입니다.
+    pass
+    print("Five is greater than two!")
+print("end")
