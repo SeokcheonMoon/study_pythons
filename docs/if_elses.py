e62473d737f54d175e0a39177681ddbd24105fdc
seokcheon <my980519@gmail.com>
2023-12-05 15:02:45 +0900

    modify

--- docs/if_elses.py
+++ docs/if_elses.py
@@ -5,18 +5,18 @@ elif True:
     pass
 else :
     pass
-    
+
     #문자 비교
-    str_name = "seokcheon MOON"
+str_name = "seokcheon MOON"
     
     # 질문형식(condition) = 변수 + 부등호 + 기준값
     #문자에 대해 긍정으로 질문
-    if str_name == "seokcheon MOON" :
-        pass
-        print("name is {}.".format(str_name))
+if str_name == "seokcheon MOON" :
+    pass
+    print("name is {}.".format(str_name))
     
-    #문자에 대해 부정으로 질문
-    if str_name != "seokcheon MOON" :
-        pass
-        print("name is not {}.".format(str_name))
-    print("End program!")
+#문자에 대해 부정으로 질문
+if str_name != "seokcheon MOON" :
+    pass
+    print("name is not {}.".format(str_name))
+print("End program!")
