cffdc6910874a43a1a12eb91c22462c353adf8bf
seokcheon <my980519@gmail.com>
2023-12-05 14:10:37 +0900

    modify folder name

--- docs/quests/asjdnas.py
+++
@@ -1,7 +0,0 @@
-num_first, num_second, num_third= input().split()
-
-first = int(num_first)
-second = int(num_second)
-third = int(num_third)
-fourth = first*second*c
-print("가로({})m * 세로({})m * 높이({}) = 직육면체({})m^3".format(first,second,third,fourth))
