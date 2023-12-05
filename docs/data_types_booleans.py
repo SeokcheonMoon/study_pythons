7e84e92987dfb8966d5bc15fbad57fb141d631fc
seokcheon <my980519@gmail.com>
2023-12-05 15:00:38 +0900

    add if else

---
+++ docs/data_types_booleans.py
@@ -0,0 +1,15 @@
+bool_flag = True
+pass
+bool_flag = False
+type(bool_flag)
+# <class 'bool'>
+
+# 문자 판단 (==:같음기호)(!=:다름기호)
+str_name = "seokcheon MOON" #변수를 a로 정의
+str_name == "seokcheon MOON" #변수가 a가 맞나요
+True                            #네
+str_name == "seokcheonMOON" #변수가 b가 맞나요
+False                           #아니요
+str_name != "seokcheonMOON" #변수가 b와 다르나요
+True                            #네
+
