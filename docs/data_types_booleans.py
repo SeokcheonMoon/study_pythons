971af657502cdef2a28c10f634a22a25147be0b0
seokcheon <my980519@gmail.com>
2023-12-05 15:53:54 +0900

    studying if else

--- docs/data_types_booleans.py
+++ docs/data_types_booleans.py
@@ -7,9 +7,39 @@ type(bool_flag)
 # 문자 판단 (==:같음기호)(!=:다름기호)
 str_name = "seokcheon MOON" #변수를 a로 정의
 str_name == "seokcheon MOON" #변수가 a가 맞나요
-True                            #네
+# True                            #네
 str_name == "seokcheonMOON" #변수가 b가 맞나요
-False                           #아니요
+# False                           #아니요
 str_name != "seokcheonMOON" #변수가 b와 다르나요
-True                            #네
+# True                            #네
 
+# 숫자에 대한 판단 (변수 + 부등호 + 기준값)
+# 컴퓨터 입장
+# True = Yes = 1, False = No = 0
+
+5 == 4
+num_first = 5
+num_first == 4
+# False
+num_first != 4
+# True
+
+num_first > 4 #초과
+# True
+num_first >= 4 #이상
+# True
+
+# 90점 이상 : A, 80점 초과 : B, 나머지는 F
+myscore = 90
+myscore >= 90
+True
+myscore = 89
+
+myscore >= 90
+False
+myscore > 80
+True
+myscore = 80
+myscore > 80
+False
+pass
--- docs/if_elses.py
+++ docs/if_elses.py
@@ -19,4 +19,38 @@ if str_name == "seokcheon MOON" :
 if str_name != "seokcheon MOON" :
     pass
     print("name is not {}.".format(str_name))
+#if - else
+#num_first >= 4 가 True 이면 : 큼, False 이면 : 작음
+num_first = 5
+if num_first > 4 :
+    pass
+    print("{}는 4보다 크다".format(num_first))
+else : 
+    pass
+    print("{}는 4보다 작다".format(num_first))
 print("End program!")
+
+num_first = 5
+if num_first > 6 :
+    pass
+    print("{}는 4보다 크다".format(num_first))
+else : 
+    pass
+    print("{}는 4보다 작다".format(num_first))
+print("End program!")
+
+# if elif else 구문
+# 점수에 따른 표시
+# 90점 이상 : A, 80점 초과 : B, 나머지는 F
+my_score = 79
+
+if my_score >= 90 :
+    pass
+    print("{}은 90점 이상이므로 A입니다.".format(my_score))
+elif my_score > 80 :
+    pass 
+    print("{}은 80점 초과이므로 B입니다.".format(my_score))
+else :
+    pass
+    print("{}은 80점 이하이므로 F입니다.".format(my_score))
+print("End program")
