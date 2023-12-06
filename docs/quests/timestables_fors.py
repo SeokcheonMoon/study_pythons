1bbcf1a10f60ddf77d4ae1e6e74839476e879976
seokcheon <my980519@gmail.com>
2023-12-06 11:34:40 +0900

    modify quest

--- docs/quests/timestables_fors.py
+++ docs/quests/timestables_fors.py
@@ -1,7 +1,7 @@
-
+num_first = int(input())
 num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
  
-for number in num_list : 
+for num_second in num_list : 
     pass
-    print("5 * {} = {}.".format(number,number*5))
+    print("{} * {} = {}.".format(num_first,num_second,num_first*num_second))
 print("End program!")
