6c71205c35180ecf184633d7415c6461b07ce97d
seokcheon <my980519@gmail.com>
2023-12-06 14:04:41 +0900

    coding test 10869

---
+++ docs/codingtest/10869.py
@@ -0,0 +1,12 @@
+num_first, num_second =  input().split()
+
+
+A = int(num_first)
+B = int(num_second)
+
+print("{}".format(A+B))
+print("{}".format(A-B))
+print("{}".format(A*B))
+print("{}".format(int(A/B)))
+print("{}".format(A%B))
+
--- docs/codingtest/note.py
+++
@@ -1,8 +0,0 @@
-A= int(input())
-B= int(input())
-
-print("{}".format(number))
-print(A-B)
-print(A*B)
-print (int(A/B))
-print(A%B)
