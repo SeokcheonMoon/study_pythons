01903ebf60261643b13ebe9b5ef4039b912e6ebe
seokcheon <my980519@gmail.com>
2023-12-06 15:49:33 +0900

    modify loops_fors.py

--- docs/loops_fors.py
+++ docs/loops_fors.py
@@ -50,6 +50,10 @@ print("End program!")
 
 # 당신의 답변 : E
 
+###############################################################################################################################################
+
+# 설문 만드는 과정
+
 list_polls = ["1. 컴퓨터 운영체제에 대한 선호도는 어떠신가요?"
             ,"A. Windows B. macOS C. Linux D. Chrome OS E. 기타"
             ,"2. 주로 사용하는 프로그래밍 언어는 무엇인가요?"
@@ -58,9 +62,24 @@ list_polls = ["1. 컴퓨터 운영체제에 대한 선호도는 어떠신가요?
             ,"A. Python B. Java C. JavaScript D. C++ E. 기타"
             ]
 
+list_statistics = [0,0,0,0,0] # 보기 갯수만큼 0을 넣어줌.
+
 for num_count in [0,2,4] : 
-    str_content= list_polls[num_count]
-    print("{}".format(str_content))
+    # str_content= list_polls[num_count]
+    # print("{}".format(str_content))
+    str_question= list_polls[num_count]
+    print("{}".format(str_question))
+    str_answer= list_polls[num_count+1]
+    print("{}".format(str_answer))
+
+    str_question_result = input("당신의 답변 (A~E를 번호로 입력) : ")
+    
+    num_question_result =  int(str_question_result)                      # 문자를 숫자로 변환
+    index = num_question_result - 1                                              #index의 위치값(사용자의 첫번째 항목은 인덱스의 0번 항목과 같다.)
+    list_statistics[index] = list_statistics[index] + 1
+
     print("---------------------------")
     pass 
-    
+
+print("선호 답항 : {}".format(list_statistics))
+print("End program!")
