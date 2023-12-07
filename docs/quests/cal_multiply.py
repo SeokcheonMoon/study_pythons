40cde51e80dd6cb687d2f192705875a5a9dd7d9c
seokcheon <my980519@gmail.com>
2023-12-07 17:51:05 +0900

    cal_multiply

---
+++ docs/quests/cal_multiply.py
@@ -0,0 +1,33 @@
+# 계산기 만들기
+
+def cal (first, second) :  
+    result = first * second
+    return result
+
+num_alpha, num_beta = input().split()
+alpha = int(num_alpha)
+beta = int(num_beta)
+
+cal_result = cal(alpha, beta)
+
+print("{}".format(cal_result))
+
+
+
+    
+
+# 반복문 구성하기
+
+
+while True :
+    pass
+    alpha,beta = input().split()
+    
+    if alpha == "q" :
+        pass
+        break
+    
+
+
+
+
