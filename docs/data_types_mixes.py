2ec433944f8a13f4652503176fcc578145a8f40b
seokcheon <my980519@gmail.com>
2023-12-12 14:50:31 +0900

    add mixes

--- docs/data_types_mixes.py
+++ docs/data_types_mixes.py
@@ -33,6 +33,11 @@ list_carinfor = [
     }
 ]
 
+# "model": "Mustang" 접근 방법
+
+list_carinfor_index_first = list_carinfor[0]
+list_carinfor_index_first["model"]
+
 pass
 dict_carinfor_k5={
     "brand": "Kia",
@@ -44,3 +49,20 @@ dict_carinfor_k5={
 
 list_carinfor.append(dict_carinfor_k5)
 pass
+
+# list_carinfor 내의 index 0번에 있는 model:Mustang 접근방법
+dict_carinfor_index_first = list_carinfor[0]
+dict_carinfor_index_first["model"]
+pass
+#또는
+list_carinfor[0]["model"]
+
+pass
+
+# for로 각 dict 정보 출력
+for dict_carfor in list_carinfor : 
+    brand = dict_carfor["brand"]
+    model =dict_carfor["model"]
+
+    print("{}{}".format(brand,model))
+    pass
