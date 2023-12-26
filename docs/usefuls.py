e483cbfacf83b49fa034195da199a022ec80a8c3
seokcheon <my980519@gmail.com>
2023-12-26 11:30:47 +0900

    useful

---
+++ docs/usefuls.py
@@ -0,0 +1,18 @@
+#range
+# ref : https://python-reference.readthedocs.io/en/latest/docs/functions/range.html#example
+# range (stop) range (start, stop[, step])
+
+# >>> range(10)
+# [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+# >>> range(1, 11)
+# [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# >>> range(0, 30, 5)
+# [0, 5, 10, 15, 20, 25]
+# >>> range(0, 10, 3)
+# [0, 3, 6, 9]
+# >>> range(0, -10, -1)
+# [0, -1, -2, -3, -4, -5, -6, -7, -8, -9]
+# >>> range(0)
+# []
+# >>> range(1, 0)
+# []
