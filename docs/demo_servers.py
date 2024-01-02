f98429ab06875f335f881f91f359e59c928b2cbd
seokcheon <my980519@gmail.com>
2024-01-02 10:22:38 +0900

    demo

---
+++ docs/demo_servers.py
@@ -0,0 +1,9 @@
+# 항시 응답 프로그램 만들기
+
+# while True : --------------------------------------------------------------------고객 응대 기본 구성
+#     work, value = input("업무 / 해당값 : ").split()
+#     print("work : {}, value : {}".format(work,value))
+
+while True : 
+    work, value = input("업무 / 해당값 : ").split()
+    print("work : {}, value : {}".format(work,value))    
