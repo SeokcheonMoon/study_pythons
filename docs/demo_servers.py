4f8570c7380e7f2f32c3d0862267dde475df100d
seokcheon <my980519@gmail.com>
2024-01-02 14:23:20 +0900

    soomething

--- docs/demo_servers.py
+++ docs/demo_servers.py
@@ -1,3 +1,11 @@
+def home() : 
+    html = "<body> It is home.</body>"
+    return html
+
+def error() : 
+    html = "<body> It is error.</body>"
+    return html
+
 # 항시 응답 프로그램 만들기
 
 # while True : --------------------------------------------------------------------고객 응대 기본 구성
@@ -7,3 +15,9 @@
 while True : 
     work, value = input("업무 / 해당값 : ").split()
     print("work : {}, value : {}".format(work,value))    
+    if work == "home" :
+        result = home()
+        print(result)
+    else :
+        result = error()
+        print(result)
