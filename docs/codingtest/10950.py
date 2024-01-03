4c53adf9a2c3edeec0ac248677d351b28b090bcf
seokcheon <my980519@gmail.com>
2024-01-03 18:41:47 +0900

    8393

---
+++ docs/codingtest/10950.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/10950
+
+# 문제
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+# 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+# 출력
+# 각 테스트 케이스마다 A+B를 출력한다.
+
+def case():
+    num_case = input()
+    case = int(num_case)
+
+    for x in range(case):
+        str_A,str_B = input().split()
+        A = int(str_A)
+        B = int(str_B)
+        print("{}".format(A+B))
+    return 0
+
+case()
