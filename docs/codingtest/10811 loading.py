1fc1d24c3b61671e4e564d19e1ef1326f67e8a61
seokcheon <my980519@gmail.com>
2024-01-25 10:23:02 +0900

    many

---
+++ docs/codingtest/10811 loading.py
@@ -0,0 +1,62 @@
+# https://www.acmicpc.net/problem/10811
+
+# 문제
+# 도현이는 바구니를 총 N개 가지고 있고, 각각의 바구니에는 1번부터 N번까지 번호가 순서대로 적혀져 있다. 바구니는 일렬로 놓여져 있고, 가장 왼쪽 바구니를 1번째 바구니, 그 다음 바구니를 2번째 바구니, ..., 가장 오른쪽 바구니를 N번째 바구니라고 부른다. 
+
+# 도현이는 앞으로 M번 바구니의 순서를 역순으로 만들려고 한다. 도현이는 한 번 순서를 역순으로 바꿀 때, 순서를 역순으로 만들 범위를 정하고, 그 범위에 들어있는 바구니의 순서를 역순으로 만든다.
+
+# 바구니의 순서를 어떻게 바꿀지 주어졌을 때, M번 바구니의 순서를 역순으로 만든 다음, 바구니에 적혀있는 번호를 가장 왼쪽 바구니부터 출력하는 프로그램을 작성하시오.
+
+# 입력
+# 첫째 줄에 N (1 ≤ N ≤ 100)과 M (1 ≤ M ≤ 100)이 주어진다.
+
+# 둘째 줄부터 M개의 줄에는 바구니의 순서를 역순으로 만드는 방법이 주어진다. 방법은 i j로 나타내고, 왼쪽으로부터 i번째 바구니부터 j번째 바구니의 순서를 역순으로 만든다는 뜻이다. (1 ≤ i ≤ j ≤ N)
+
+# 도현이는 입력으로 주어진 순서대로 바구니의 순서를 바꾼다.
+
+# 출력
+# 모든 순서를 바꾼 다음에, 가장 왼쪽에 있는 바구니부터 바구니에 적혀있는 순서를 공백으로 구분해 출력한다.
+
+# 5 4
+# 1 2
+# 3 4
+# 1 4
+# 2 2
+
+
+# 12345
+# 21345
+# 21435
+# 34125
+
+str_total, str_bucket = input().split()
+total = int(str_total)          #5
+bucket = int(str_bucket)        #4
+
+
+
+
+
+list_bucket = []
+for x in range(total) :
+    list_bucket.append(x+1)
+
+print(list_bucket)
+
+
+
+
+list_new = []
+for y in range(bucket) :
+    str_first, str_second = input().split()   #1 4
+    first = int(str_first)
+    second = int(str_second)
+
+    set= list_bucket[first:second]
+    set.reverse()
+    # refirst = first
+    # list_bucket[first] = list_bucket[second]
+    # list_bucket[second] = refirst
+
+print(set)
+    
