6aed62215ec3164cdf701bfbcec62c1b5c26b51a
seokcheon <my980519@gmail.com>
2024-01-26 11:29:41 +0900

    1152

--- docs/codingtest/1152.py
+++ docs/codingtest/1152.py
@@ -26,4 +26,12 @@ str_text = input()
 
 text = list(str_text)
 
+
+if text[0] ==" " and text[len(text)-1] == " " :
+    count_blank = text.count(" ")-1
+elif text[0] ==" " or text[len(text)-1] == " " : 
+    count_blank = text.count(" ")
+else :
+    count_blank = text.count(" ")+1
+
 print(count_blank)
